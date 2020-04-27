1383e567e3001b07123c0dff4139879dd51ab765
morcodou <morcodou@home>
2020-04-26 21:41:33 -0400

    move all variables close to their first usage

--- CodeLuau/Speaker.cs
+++ CodeLuau/Speaker.cs
@@ -28,11 +28,6 @@ namespace CodeLuau
 		public RegisterResponse Register(IRepository repository)
 		{
 			int? speakerId = null;
-			bool good = false;
-			bool appr = false;
-			var ot = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
-
-			var domains = new List<string>() { "aol.com", "prodigy.com", "compuserve.com" };
 
 			if (!string.IsNullOrWhiteSpace(FirstName))
 			{
@@ -43,12 +38,13 @@ namespace CodeLuau
 						//put list of employers in array
 						var emps = new List<string>() { "Pluralsight", "Microsoft", "Google" };
 
-						good = Exp > 10 || HasBlog || Certifications.Count() > 3 || emps.Contains(Employer);
+						bool good = Exp > 10 || HasBlog || Certifications.Count() > 3 || emps.Contains(Employer);
 
 						if (!good)
 						{
 							//need to get just the domain from the email
 							string emailDomain = Email.Split('@').Last();
+							var domains = new List<string>() { "aol.com", "prodigy.com", "compuserve.com" };
 
 							if (!domains.Contains(emailDomain) && (!(Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9)))
 							{
@@ -58,8 +54,10 @@ namespace CodeLuau
 
 						if (good)
 						{
+							bool appr = false;
 							if (Sessions.Count() != 0)
 							{
+								var ot = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
 								foreach (var session in Sessions)
 								{
 									foreach (var tech in ot)
