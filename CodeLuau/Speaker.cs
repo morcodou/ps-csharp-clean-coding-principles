a5d784adf237b294c8ae57e1c6bb5969ef2fbeae
morcodou <morcodou@home>
2020-04-26 22:59:41 -0400

    centralize validation

--- CodeLuau/Speaker.cs
+++ CodeLuau/Speaker.cs
@@ -27,35 +27,25 @@ namespace CodeLuau
         /// <returns>speakerID</returns>
         public RegisterResponse Register(IRepository repository)
         {
-            int? speakerId = null;
-
-            var error = ValidateData();
+            var error = ValidateRegistration();
             if (error != null) return new RegisterResponse(error);
 
+            int? speakerId = repository.SaveSpeaker(this);
+            return new RegisterResponse((int)speakerId);
+        }
+
+        private RegisterError? ValidateRegistration()
+        {
+            var error = ValidateData();
+            if (error != null) return error;
+
             bool speakerAppearsQualified = AppearsExceptional() || !HasObviousRedFlags();
-            if (!speakerAppearsQualified) return new RegisterResponse(RegisterError.SpeakerDoesNotMeetStandards);
+            if (!speakerAppearsQualified) return RegisterError.SpeakerDoesNotMeetStandards;
 
             bool atLeastOneSessionApprroved = ApproveSessions();
+            if (!atLeastOneSessionApprroved) return RegisterError.NoSessionsApproved;
 
-            if (atLeastOneSessionApprroved)
-            {
-                //Now, save the speaker and sessions to the db.
-                try
-                {
-                    speakerId = repository.SaveSpeaker(this);
-                }
-                catch (Exception e)
-                {
-                    //in case the db call fails 
-                }
-            }
-            else
-            {
-                return new RegisterResponse(RegisterError.NoSessionsApproved);
-            }
-
-            //if we got this far, the speaker is registered.
-            return new RegisterResponse((int)speakerId);
+            return null;
         }
 
         private bool ApproveSessions()
